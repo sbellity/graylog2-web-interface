5cfe811ec03ddcfd5239e05590494e91d92abfad
Lennart Koopmann <lennart@socketfeed.com>
2010-10-29 21:29:13 +0200

    lol first parts of new dashboard

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
-    $('#problems').anythingSlider({
-            easing: "swing",                // Anything other than "linear" or "swing" requires the easing plugin
-            autoPlay: true,                 // This turns off the entire FUNCTIONALY, not just if it starts running or not
-            startStopped: false,            // If autoPlay is on, this can force it to start stopped
-            delay: 3000,                    // How long between slide transitions in AutoPlay mode
-            animationTime: 600,             // How long the slide transition takes
-            hashTags: true,                 // Should links change the hashtag in the URL?
-            buildNavigation: false,          // If true, builds and list of anchor links to link to each slide
-            pauseOnHover: true,             // If true, and autoPlay is enabled, the show will pause on hover
-            startText: "",             // Start text
-            stopText: "",               // Stop text
-            navigationFormatter: null       // Details at the top of the file on this use (advanced use)
+    WorldMap({
+        id: "worldmap",
+        bgcolor: "#1F242A",
+        fgcolor: "#37414A",
+        bordercolor: "1F242A",
+        borderwidth: 0.5,
+        padding: 10,
+        detail: {
+            "de": "#62414a",
+            "es": "#62414a",
+            "fr": "#62414a",
+            "gb": "#c5414a",
+            "nl": "#c5414a",
+            "be": "#c5414a",
+            "cn": "#ec414a"
+        }
     });
 });
